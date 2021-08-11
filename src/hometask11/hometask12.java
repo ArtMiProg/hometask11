b682306e6a51ff273e38e532d8e3a06bd1a0a28b
ArtMiProg <am.poshta1@gmail.com>
2021-08-11 16:52:50 +0300

    To GitHub

--- src/hometask11/hometask12.java
+++ src/hometask11/hometask12.java
@@ -11,30 +11,36 @@ public class hometask12 {
     public static void main(String[] args) {
         System.out.println("Enter a value");
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        System.out.println("Enter b value");
-        int b = sc.nextInt();
-        System.out.println("Enter c value");
-        int c = sc.nextInt();
-        // Так как сказано, что диапазона должно быть три, то а не может быть равно b.
-        if (a < b) {
-            if (c <= a) {
-                System.out.println("c<=a<b");
-            } else if (c > a && c < b) {
-                System.out.println("a<c<b");
-            } else if (c >= b) {
-                System.out.println("a<b<=c");
+        if (sc.hasNextInt()) {
+            int a = sc.nextInt();
+            System.out.println("Enter b value");
+            if (sc.hasNextInt()) {
+                int b = sc.nextInt();
+                System.out.println("Enter c value");
+                if (sc.hasNextInt()) {
+                    int c = sc.nextInt();
+                    // Так как сказано, что диапазона должно быть три, то а не может быть равно b.
+                    if (a < b) {
+                        if (c <= a) {
+                            System.out.println("c<=a<b");
+                        } else if (c > a && c < b) {
+                            System.out.println("a<c<b");
+                        } else if (c >= b) {
+                            System.out.println("a<b<=c");
+                        }
+                    } else if (b < a) {
+                        if (c <= b) {
+                            System.out.println("c<=b<a");
+                        } else if (c > b && c < a) {
+                            System.out.println("b<c<a");
+                        } else if (c >= a) {
+                            System.out.println("b<a<=c");
+                        }
+                    } else if (b == a) {
+                        System.out.println("Incorrect data");
+                    }
+                }
             }
-        } else if (b < a) {
-            if (c <= b) {
-                System.out.println("c<=b<a");
-            } else if (c > b && c < a) {
-                System.out.println("b<c<a");
-            } else if (c >= a) {
-                System.out.println("b<a<=c");
-            }
-        } else if (b == a) {
-            System.out.println("Incorrect data");
         }
     }
 }
