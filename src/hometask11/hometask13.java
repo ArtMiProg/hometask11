55819fcb3617a53c6c7695191a772828e08d1e01
ArtMiProg <am.poshta1@gmail.com>
2021-08-11 16:48:10 +0300

    To GitHub

--- src/hometask11/hometask13.java
+++ src/hometask11/hometask13.java
@@ -10,36 +10,42 @@ public class hometask13 {
     public static void main(String[] args) {
         System.out.println("Enter a value");
         Scanner sc = new Scanner(System.in);
-        float a = sc.nextFloat();
-        System.out.println("Enter b value");
-        float b = sc.nextFloat();
-        System.out.println("Enter c value");
-        float c = sc.nextFloat();
-        if (a > b) {
-            if (b > c) {
-                System.out.println("Max num is a = " + a);
-                System.out.println("Min num is c = " + c);
-            } else if (c > a) {
-                System.out.println("Max num is c = " + c);
-                System.out.println("Min num is b = " + b);
-            } else {
-                System.out.println("Max num is a = " + a);
-                System.out.println("Min num is b = " + b);
-            }
-        } else if (b > a) {
-            if (a > c) {
-                System.out.println("Max num is b = " + b);
-                System.out.println("Min num is c = " + c);
-            } else if (c > b) {
-                System.out.println("Max num is c = " + c);
-                System.out.println("Min num is a = " + a);
-            } else {
-                System.out.println("Max num is b = " + b);
-                System.out.println("Min num is a = " + a);
+        if (sc.hasNextInt()) {
+            float a = sc.nextFloat();
+            System.out.println("Enter b value");
+            if (sc.hasNextInt()) {
+                float b = sc.nextFloat();
+                System.out.println("Enter c value");
+                if (sc.hasNextInt()) {
+                    float c = sc.nextFloat();
+                    if (a > b) {
+                        if (b > c) {
+                            System.out.println("Max num is a = " + a);
+                            System.out.println("Min num is c = " + c);
+                        } else if (c > a) {
+                            System.out.println("Max num is c = " + c);
+                            System.out.println("Min num is b = " + b);
+                        } else {
+                            System.out.println("Max num is a = " + a);
+                            System.out.println("Min num is b = " + b);
+                        }
+                    } else if (b > a) {
+                        if (a > c) {
+                            System.out.println("Max num is b = " + b);
+                            System.out.println("Min num is c = " + c);
+                        } else if (c > b) {
+                            System.out.println("Max num is c = " + c);
+                            System.out.println("Min num is a = " + a);
+                        } else {
+                            System.out.println("Max num is b = " + b);
+                            System.out.println("Min num is a = " + a);
+                        }
+                    }
+                    float arithMean = 0;
+                    System.out.println("arithMean = " + (a + b + c) / 3);
+                }
             }
         }
-        float arithMean = 0;
-        System.out.println("arithMean = " + (a + b + c) / 3);
     }
 }
 
