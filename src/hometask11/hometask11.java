8e3183c6d2cf692eb511e2ad64697f9dda50550f
ArtMiProg <am.poshta1@gmail.com>
2021-08-11 16:55:12 +0300

    To GitHub

--- src/hometask11/hometask11.java
+++ src/hometask11/hometask11.java
@@ -10,16 +10,18 @@ public class hometask11 {
     public static void main(String[] args) {
         System.out.println("Enter day Number");
         Scanner sc = new Scanner(System.in);
-        int dayNumber = sc.nextInt();
-        switch (dayNumber) {
-            case 1 -> System.out.println("Monday");
-            case 2 -> System.out.println("Tuesday");
-            case 3 -> System.out.println("Wednesday");
-            case 4 -> System.out.println("Thursday");
-            case 5 -> System.out.println("Friday");
-            case 6 -> System.out.println("Saturday");
-            case 7 -> System.out.println("Sunday");
-            default -> System.out.println("There is no day with such number");
+        if (sc.hasNextInt()) {
+            int dayNumber = sc.nextInt();
+            switch (dayNumber) {
+                case 1 -> System.out.println("Monday");
+                case 2 -> System.out.println("Tuesday");
+                case 3 -> System.out.println("Wednesday");
+                case 4 -> System.out.println("Thursday");
+                case 5 -> System.out.println("Friday");
+                case 6 -> System.out.println("Saturday");
+                case 7 -> System.out.println("Sunday");
+                default -> System.out.println("There is no day with such number");
+            }
         }
     }
 }
